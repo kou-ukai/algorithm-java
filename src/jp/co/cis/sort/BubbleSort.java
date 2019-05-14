3952cbc531357daccffd33784a2e0d85479a95c1
kou.ukai <kou.ukai@cisys.co.jp>
2019-05-14 19:33:53 +0900

    解答例

--- src/jp/co/cis/sort/BubbleSort.java
+++ src/jp/co/cis/sort/BubbleSort.java
@@ -8,12 +8,39 @@ public class BubbleSort {
 
 	/**
 	 * 配列の値を小さい順に並び替えた結果を返却します。
+	 * 
 	 * @param ary
 	 * @return
 	 */
 	public int[] sort(int[] ary) {
-		// TODO バブルソートで並び替える処理を実装してください。
-		return ary;
+
+		// ソート結果格納先
+		int[] ans = ary.clone();
+
+		// 添え字での最大値
+		final int max = ans.length - 1;
+
+		// 比較下限を先頭からカウントアップ
+		for (int i = 0; i < max; i++) {
+
+			// 比較対象を後ろの要素からカウントダウン
+			for (int j = max; j > i; j--) {
+
+				if (ans[j] < ans[j - 1]) {
+
+					// 後ろの要素[j]が前の要素[j - 1]より小さい場合、
+					// 前の要素と交換する
+					int temp = ans[j];
+					ans[j] = ans[j - 1];
+					ans[j - 1] = temp;
+
+				}
+
+			}
+
+		}
+
+		return ans;
 	}
 
 }
