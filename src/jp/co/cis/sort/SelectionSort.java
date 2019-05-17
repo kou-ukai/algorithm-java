8504ed82e254f020f1b5b07eacb8530437f14324
kou.ukai <kou.ukai@cisys.co.jp>
2019-05-17 18:11:42 +0900

    選択ソート追加

---
+++ src/jp/co/cis/sort/SelectionSort.java
@@ -0,0 +1,19 @@
+package jp.co.cis.sort;
+
+/**
+ * 選択ソートで配列の値を並び替えるクラスです。
+ *
+ */
+public class SelectionSort {
+
+	/**
+	 * 配列の値を小さい順に並び替えた結果を返却します。
+	 * @param ary
+	 * @return
+	 */
+	public int[] sort(int[] ary) {
+		// TODO 選択ソートで並び替える処理を実装してください。
+		return ary;
+	}
+
+}
