3de6185dfce64b3a085af92f010df6f6feb1bb35
=AotoTakeyame <aoto.takeyama@cisys.co.jp>
2019-05-24 10:16:01 +0900

    sort

--- src/jp/co/cis/sort/SelectionSort.java
+++ src/jp/co/cis/sort/SelectionSort.java
@@ -12,7 +12,33 @@ public class SelectionSort {
 	 * @return
 	 */
 	public int[] sort(int[] ary) {
-		// TODO 選択ソートで並び替える処理を実装してください。
+		//受け取る配列の長さ
+		int aryLength = ary.length;
+		//最大値を探す処理をする回数
+		int changeNum = aryLength - 1;
+
+		for (int i = 1; i <= changeNum; i++) {
+			//左端(最大値を入れる場所)のindex番号
+			int tempAryIndex = i - 1;
+			//最大値があるindex番号を受け取る
+			int maxAryIndex = i - 1;
+			//最大値を受け取る
+			int maxAryNum = ary[i - 1];
+
+			//最大値をもつindex番号を調べる
+			for (int j = tempAryIndex; j <= changeNum; j++) {
+				if (maxAryNum < ary[j]) {
+					maxAryNum = ary[j];
+					maxAryIndex = j;
+				}
+			}
+
+			//左端と最大値を持つ配列要素を入れ替える
+			int tempNum = ary[tempAryIndex];
+			ary[tempAryIndex] = ary[maxAryIndex];
+			ary[maxAryIndex] = tempNum;
+		}
+
 		return ary;
 	}
 
