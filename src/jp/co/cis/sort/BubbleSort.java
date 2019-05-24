ac19af2577de4a72777b087376631c984c3c07cc
AotoTakeyama <50577559+AotoTakeyama@users.noreply.github.com>
2019-05-24 10:31:00 +0900

    update SelectionSort.java
    sortメソッドを実装

--- src/jp/co/cis/sort/BubbleSort.java
+++ src/jp/co/cis/sort/BubbleSort.java
@@ -1,10 +1,10 @@
 package jp.co.cis.sort;
 
 /**
- * バブルソートで並び替えるクラスです。
+ * 選択ソートで配列の値を並び替えるクラスです。
  *
  */
-public class BubbleSort {
+public class SelectionSort {
 
 	/**
 	 * 配列の値を小さい順に並び替えた結果を返却します。
@@ -12,23 +12,34 @@ public class BubbleSort {
 	 * @return
 	 */
 	public int[] sort(int[] ary) {
-		
-	/**
-	 * aryLengthで配列の長さを取得
-	 * changeNumは配列の入れ替えをする回数を表します
-	 */
-	    int aryLength = ary.length;
-	    int changeNum = aryLength - 1;
-	      for(int i=changeNum; i>0; i--){
-		for(int j=0; j<i; j++){
-		  if(ary[j]>ary[j+1]){
-		    int temNum = ary[j+1];
-		    ary[j+1] = ary[j];
-		    ary[j] = temNum;
-		  }
+		//受け取る配列の長さ
+		int aryLength = ary.length;
+		//最大値を探す処理をする回数
+		int changeNum = aryLength - 1;
+
+		for (int i = 1; i <= changeNum; i++) {
+			//左端(最大値を入れる場所)のindex番号
+			int tempAryIndex = i - 1;
+			//最大値があるindex番号を受け取る
+			int maxAryIndex = i - 1;
+			//最大値を受け取る
+			int maxAryNum = ary[i - 1];
+
+			//最大値をもつindex番号を調べる
+			for (int j = tempAryIndex; j <= changeNum; j++) {
+				if (maxAryNum < ary[j]) {
+					maxAryNum = ary[j];
+					maxAryIndex = j;
+				}
+			}
+
+			//左端と最大値を持つ配列要素を入れ替える
+			int tempNum = ary[tempAryIndex];
+			ary[tempAryIndex] = ary[maxAryIndex];
+			ary[maxAryIndex] = tempNum;
 		}
-	      }
-	    return ary;
+
+		return ary;
 	}
 
 }
