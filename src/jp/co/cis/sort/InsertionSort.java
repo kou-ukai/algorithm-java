cbb8d2002985042a76afddb7bab91b9d40e2bf61
shodakenta <50577699+shodakenta@users.noreply.github.com>
2019-05-30 08:37:27 +0900

    InsertionSort
    コード記述完了

--- src/jp/co/cis/sort/InsertionSort.java
+++ src/jp/co/cis/sort/InsertionSort.java
@@ -13,6 +13,15 @@ public class InsertionSort {
 	 */
 	public int[] sort(int[] ary) {
 		// TODO 挿入ソートで並び替える処理を実装してください。
+		for(int i = 1; i < ary.length-1; i++){
+			int tmp = ary[i];
+			for(int j = i; j > 0; j--){
+				if(ary[j] > ary[j-1]){
+					ary[j] = ary[j-1];
+					ary[j-1] = tmp;
+				}
+			}
+		}
 		return ary;
 	}
 
