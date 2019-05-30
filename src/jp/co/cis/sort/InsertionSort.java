c77f4be8e56165cea19bce47296713b096e3b729
shodakenta <50577699+shodakenta@users.noreply.github.com>
2019-05-30 18:36:15 +0900

    Update InsertionSort.java

--- src/jp/co/cis/sort/InsertionSort.java
+++ src/jp/co/cis/sort/InsertionSort.java
@@ -14,12 +14,11 @@ public class InsertionSort {
 	public int[] sort(int[] ary) {
 		// TODO 挿入ソートで並び替える処理を実装してください。
 		for(int i = 1; i < ary.length-1; i++){
-		// 比較する配列のインデックス
-			int tmp = i
+		// 比較する配列のインデックスをiとする
 			int tmp = ary[i];
 			// ary[0]まで比較を繰り返す
 			for(int j = i; j > 0; j--){
-				//左辺 > 右辺のとき、入れ替える
+				//（左辺 > 右辺）のとき、入れ替える
 				if(ary[j] > ary[j-1]){
 					ary[j] = ary[j-1];
 					ary[j-1] = tmp;
