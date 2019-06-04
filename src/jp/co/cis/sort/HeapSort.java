e59f99c84c7bd8ff69575feace0d568869518572
kou.ukai <kou.ukai@cisys.co.jp>
2019-06-04 09:39:42 +0900

    ヒープソート回答例作成

--- src/jp/co/cis/sort/HeapSort.java
+++ src/jp/co/cis/sort/HeapSort.java
@@ -23,7 +23,9 @@ public class HeapSort {
 		for (int i = max; i > 0; i--) {
 
 			// TODO 先頭（未ソート範囲での最大値）とソート位置を入れ替える
-
+			int temp = ary[i - 1];
+			ary[i - 1] = ary[0];
+			ary[0] = temp;
 
 			// ヒープの整理
 			downHeap(ary, i);
@@ -38,7 +40,27 @@ public class HeapSort {
 	 * @param ary
 	 */
 	private void initHeap(int[] ary) {
-		// TODO
+		final int max = ary.length;
+
+		for (int i = 1; i < max + 1; i++) {
+
+			int ko = i;
+			int oya = ko / 2;
+			while (oya > 0) {
+
+				if (ary[oya - 1] < ary[ko - 1]) {
+					int temp = ary[oya - 1];
+					ary[oya - 1] = ary[ko - 1];
+					ary[ko - 1] = temp;
+				} else {
+					break;
+				}
+
+				ko = oya;
+				oya = oya / 2;
+
+			}
+		}
 	}
 
 	/**
@@ -47,6 +69,34 @@ public class HeapSort {
 	 * @param max
 	 */
 	private void downHeap(int[] ary, int max) {
-		// TODO 
+		int oya = 1;
+		while (oya * 2 < max) {
+			// 子がいる限り続行する
+
+			int koLeft = oya * 2;
+			int koRight = oya * 2 + 1;
+
+			// 左右の子のうち、大きい子を選ぶ
+			int ko;
+			if (koRight < max && ary[koRight - 1] > ary[koLeft - 1]) {
+				// 右の子が比較対象であり、かつ左の子より大きい場合、
+				ko = koRight;
+			} else {
+				ko = koLeft;
+			}
+
+			// 子のほうが大きければ、入れ替える
+			if (ary[ko - 1] > ary[oya - 1]) {
+				int temp = ary[oya - 1];
+				ary[oya - 1] = ary[ko - 1];
+				ary[ko - 1] = temp;
+			} else {
+				// 子のほうが小さければ整理完了
+				break;
+			}
+
+			// 次の親位置を設定
+			oya = ko;
+		}
 	}
 }
